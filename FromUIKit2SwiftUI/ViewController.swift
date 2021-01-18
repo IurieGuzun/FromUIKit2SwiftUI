cf4ffa8ea8aaeac8d5b38e89cffcf9fdf85b43d8
Iurie Guzun <iurie_guzun@yahoo.com>
2021-01-18 12:17:00 -0500

    Before converting to SwiftUI

--- FromUIKit2SwiftUI/ViewController.swift
+++ FromUIKit2SwiftUI/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var MyButton: UIButton!
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        print("Button was pressed!")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
