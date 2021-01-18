fd36fca9815c3f362f0a51b6fc9076213e1d1bf9
Iurie Guzun <iurie_guzun@yahoo.com>
2021-01-18 12:10:45 -0500

    Initial Commit

---
+++ FromUIKit2SwiftUI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FromUIKit2SwiftUI
+//
+//  Created by Iurie Guzun on 2021-01-18.
+//  Copyright © 2021 Iurie Guzun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
